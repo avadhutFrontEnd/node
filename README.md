f4fb5a47cddca4aa70a78b19ccb7b701959373c2
Avadhut Jagde <avadhutjagde555@gmail.com>
2024-12-23 13:54:48 +0530

    [Course: The Complete Node js Course > 4-Building RESTful API's Using Express(56m) ] [ Video: #13-Handling-HTTP-Delete-Requests_mp4_4m_44s ] - Implement a new 'DELETE' endpoint to 'delete a course'.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Node project repo `node` 
 
 # Commit message format : 
-[Course: The Complete Node js Course > 4-Building RESTful API's Using Express(56m) ] [ Video: #12-Handling-HTTP-PUT-Requests_mp4_8m_30s ] - Implement a new `PUT` endpoint to 'update a course'.
+[Course: The Complete Node js Course > 4-Building RESTful API's Using Express(56m) ] [ Video: #13-Handling-HTTP-Delete-Requests_mp4_4m_44s ] - Implement a new `DELETE` endpoint to 'delete a course'.
 
 
 # my-github Account : 
--- express-demo/index.js
+++ express-demo/index.js
@@ -29,11 +29,7 @@ app.post("/api/courses", (req, res) => {
   // Validate
   const { error } = validateCourse(req.body);
   // If invalid, return 400 - Bad request
-  if (error) {
-    // 400 Bad Request
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   const course = {
     id: courses.length + 1,
@@ -47,21 +43,31 @@ app.put("/api/courses/:id", (req, res) => {
   // Look up the course
   // If not existing, return 404
   const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course)
-    res.status(404).send("The course with the given ID was not found.");
+  if (!course) return res.status(404).send("The course with the given ID was not found.");
 
   // Validate
   const { error } = validateCourse(req.body);
   // If invalid, return 400 - Bad request
-  if (error) {
-    // 400 Bad Request
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
+    
 
   // Update course
   course.name = req.body.name;
   // Return the updated course
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  // Look up the course
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  // Not existing, return 404
+  if (!course) return res.status(404).send("The course with the given ID was not found.");
+
+  // Delete
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  // Return the same course
   res.send(course);
 });
 
@@ -75,8 +81,7 @@ function validateCourse(course) {
 
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course)
-    res.status(404).send("The course with the given ID was not found.");
+  if (!course) return res.status(404).send("The course with the given ID was not found.");
   res.send(course);
 });
 
