aec94d34ea8a53a34d0699992320d6048aba35e5
Avadhut Jagde <avadhutjagde555@gmail.com>
2024-12-23 06:43:47 +0530

    [Course: The Complete Node js Course > 4-Building RESTful API's Using Express(56m) ] [ Video: #7-Route-Parameters_mp4_3m_22s ] - How to read Route-Parameters and Query String Parameters.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Node project repo `node` 
 
 # Commit message format : 
-[Course: The Complete Node js Course > 4-Building RESTful API's Using Express(56m) ] [ Video: #6-Environment-Variables_mp4_3m_13s ] - 
+[Course: The Complete Node js Course > 4-Building RESTful API's Using Express(56m) ] [ Video: #7-Route-Parameters_mp4_3m_22s ] - How to read `Route-Parameters` and `Query String Parameters`.
 
 
 # my-github Account : 
--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,6 @@
 // index.js
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // app.get()
@@ -8,14 +8,23 @@ const app = express();
 // app.put()
 // app.delete()
 
-
 // ************ Define a Route ************
-app.get('/', (req, res) => {
-	res.send('Hello World');
-})
-
-app.get('/api/courses', (req, res) => {
-	res.send([1, 2, 3]);
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
+
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+app.get("/api/posts/:year/:month", (req, res) => {
+//   res.send(req.params.year);
+  res.send(req.query);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
