951d76e427f367e62cbc792ee7186c35c73e2c70
Avadhut Jagde <avadhutjagde555@gmail.com>
2025-01-24 21:45:39 +0530

    [Course: The Complete Node js Course > 7-CRUD operations using mongoose(1h) ] [ Video: #6-Models_mp4_4m_18s ] - compile this 'schema' into a 'model'

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Node project repo `node` 
 
 # Commit message format : 
-[Course: The Complete Node js Course > 7-CRUD operations using mongoose(1h) ] [ Video: #5-Schemas_mp4_3m_48s ] - How to create a `schema`
+[Course: The Complete Node js Course > 7-CRUD operations using mongoose(1h) ] [ Video: #6-Models_mp4_4m_18s ] - compile this `schema` into a `model`
 
 
 # my-github Account : 
--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -12,3 +12,14 @@ const courseSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
 });
+
+// once we have a "schema", we need to compile that into a "model", which gives us a "Class" :
+const Course = mongoose.model("course", courseSchema);
+// Next, we can create an "object" based on that "Class" and this "object" maps to a "document" in a MongoDB database
+const course = new Course({
+  // we pass an "object" to initialize our "course" object
+  name: "Node.js Course",
+  author: "Mosh",
+  tags: ["node", "backend"],
+  isPublished: true
+});
