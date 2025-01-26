8172bd21b7cafb6311de4b61edb7e2d890bbd042
Avadhut Jagde <avadhutjagde555@gmail.com>
2025-01-26 13:53:11 +0530

    [Course: The Complete Node js Course > 7-CRUD operations using mongoose(1h) ] [ Video: #10-Logical-Query-Operators_mp4_2m_01s ] -

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Node project repo `node` 
 
 # Commit message format : 
-[Course: The Complete Node js Course > 7-CRUD operations using mongoose(1h) ] [ Video: #9-Comparison-Query-Operators_mp4_4m_59s ] - 
+[Course: The Complete Node js Course > 7-CRUD operations using mongoose(1h) ] [ Video: #10-Logical-Query-Operators_mp4_2m_01s ] - 
 
 
 # my-github Account : 
--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -45,9 +45,16 @@ async function getCourses() {
   const courses = await Course
     // .find({ author: "Mosh", isPublished: true })
     // .find({ price: 10 })
+    // *********** Comparison Operators :
     // .find({ price: { $gt: 10 } })
     // .find({ price: { $gte: 10, $lte: 20 } })\
-    .find({ price: { $in: [10, 15, 20] } })
+    // .find({ price: { $in: [10, 15, 20] } })
+    // *********** Logical Query Operators :
+    // or
+    // and
+    .find()
+    .or([{ author: "Mosh" }, { isPublished: true }])
+    .and([ ])
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
